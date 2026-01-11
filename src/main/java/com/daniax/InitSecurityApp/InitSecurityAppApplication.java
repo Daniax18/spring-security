4d49b16640617eb4a93c220e083ef50869b7ca1f
Dania Midera <daniamidera@gmail.com>
2026-01-11 23:36:27 +0400

    main: first commit

---
+++ src/main/java/com/daniax/InitSecurityApp/InitSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.daniax.InitSecurityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitSecurityAppApplication.class, args);
+	}
+
+}
